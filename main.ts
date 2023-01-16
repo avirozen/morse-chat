c352cd774cb63d72d2b215a85287fc62004cfe47
avirozen <117524971+avirozen@users.noreply.github.com>
2023-01-16 06:49:49 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
         music.playTone(262, music.beat(BeatFraction.Quarter))
-    } else {
+    } else if (receivedNumber == 1) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -28,11 +28,35 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
         music.playTone(262, music.beat(BeatFraction.Whole))
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 500, 500, 255, 0, 50, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
     }
     basic.clearScreen()
 })
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     radio.sendNumber(1)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(3)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendNumber(0)
